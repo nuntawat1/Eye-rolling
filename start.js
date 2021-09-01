acad0bf86433c5a37077bd30b0ca09987e96b5f5
unknown <nuntawat222542@gmail.com>
2021-09-01 16:54:33 +0700

    set eyecontroler

--- start.js
+++ start.js
@@ -3,6 +3,14 @@
     const eyeElems = document.querySelectorAll('.eye');
 
     function onMouseMove({ pageX, pageY }) {
+        eyeElems.forEach((eyeElem) => {
+            const { left, top } = eyeElem.getBoundingClientRect();
+
+            const eyeCenterX = left + eyeElem.offsetWidth / 2;
+            const eyeCenterY = top + eyeElem.offsetHeigth / 2;
+            const radian = Math.atan2(pageX - eyeCenterX, pageY - eyeCenterY);
+            const angle = radian * 180 / Math.PI * -1;
+            eyeElem.style.transform = `rotate(${angle}deg)`;
     }
     function run() {
     }
