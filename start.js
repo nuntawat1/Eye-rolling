6b0f700c247f5cbe4c8aff59b0a24cf61013946f
unknown <nuntawat222542@gmail.com>
2021-09-01 16:53:37 +0700

    createfunctiononmousemve

--- start.js
+++ start.js
@@ -1,6 +1,9 @@
 (() => {
     const bodyElem = document.querySelector('body');
     const eyeElems = document.querySelectorAll('.eye');
+
+    function onMouseMove({ pageX, pageY }) {
+    }
     function run() {
     }
 
